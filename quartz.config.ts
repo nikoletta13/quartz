a45e33c736aed0b7713d1eac6e607f7d7bdf005b
nikoletta13 <105713608+nikoletta13@users.noreply.github.com>
2023-11-09 09:57:35 +0000

    Quartz sync: Nov 9, 2023, 9:57 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "This Is Also A Game",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "nikoletta13.github.io/thisisalsoagame",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
